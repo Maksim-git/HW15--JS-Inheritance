4c7db7f4dbbf1e3fe14fcdc78f58c51c21a1e3e5
Maksim-git <dv.maksim11@gmail.com>
2022-06-02 10:08:01 +0300

    The constructor made the function methods in the prototype. Inherited from the prototype. Polymorphism

--- script.js
+++ script.js
@@ -2,34 +2,42 @@ const fullTime = document.querySelector("#full-time");
 const shortTime = document.querySelector("#short-time");
 
 function Clock(classTime, isClicked) {
+  this.classTime = classTime;
   this.isClicked = isClicked;
-  this.clickedChengeFormat = function () {
-    this.isClicked = !this.isClicked;
-  };
-  this.formatTime = function () {
-    if (this.isClicked) {
-      classTime.innerHTML = new Date().toLocaleTimeString();
-    } else {
-      classTime.innerHTML = new Date().toLocaleTimeString().substring(0, 5);
-    }
-  };
   classTime.addEventListener("click", () => {
     this.clickedChengeFormat();
   });
-
   setInterval(() => {
     this.formatTime();
   }, 1000);
 }
 
+Clock.prototype.clickedChengeFormat = function () {
+  this.isClicked = !this.isClicked;
+};
+
+Clock.prototype.formatTime = function () {
+  if (this.isClicked) {
+    this.classTime.innerHTML = new Date().toLocaleTimeString();
+  } else {
+    this.classTime.innerHTML = new Date().toLocaleTimeString().substring(0, 5);
+  }
+};
+
 function FullFormatTime(time) {
   Clock.call(this, time, true);
 }
+
+FullFormatTime.prototype = Object.create(Clock.prototype);
+FullFormatTime.prototype.constructor = FullFormatTime;
 
 const fullFormatTime = new FullFormatTime(fullTime);
 
 function ShotFormatTime(time) {
   Clock.call(this, time);
 }
+
+ShotFormatTime.prototype = Object.create(Clock.prototype);
+ShotFormatTime.prototype.constructor = ShotFormatTime;
 
 const shotFormatTime = new ShotFormatTime(shortTime);
